80947a20fe39cb3f47bb40c133d54de7d2d01cb4
Hassan Ahmed <hassanshakeel381@gmail.com>
2024-12-20 23:07:06 +0500

    nodemailer completed

--- src/controllers/studentContorller.js
+++ src/controllers/studentContorller.js
@@ -1,5 +1,17 @@
 import Student from "../models/studentModel.js";
 import Course from "../models/courseModel.js";
+import mongoose from "mongoose";
+import nodemailer from "nodemailer";
+
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.ethereal.email',
+  port: 587,
+  auth: {
+      user: 'emmalee.ohara68@ethereal.email',
+      pass: 'mrwedur9dam8AYQYrn'
+  }
+});
 
 const addStudent = async (req, res) => {
   const { fullName, email, enrolledCourse } = req.body;
@@ -17,10 +29,48 @@ const addStudent = async (req, res) => {
     $push: { enrolledStudents: student._id },
   });
 
-  res.json({message: "added successfully"})
+  res.json({ message: "added successfully" });
 };
 
+const getStudent = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ error: "Not valid Id" });
+  }
 
+  const student = await Student.findById(id).populate("enrolledCourse");
+  if (!student) {
+    res.status(404).json({
+      message: "no todo found!",
+    });
+    return;
+  }
 
+  res.status(200).json(student);
+};
 
-export  {addStudent }
+// pagination
+const getAllStudent = async (req, res) => {
+  const page = req.query.page || 1;
+  const limit = req.query.limit || 2;
+
+  const skip = (page - 1) * limit;
+
+  const students = await Student.find({}).skip(skip).limit(limit);
+  res.json({ data: students, length: students.length });
+};
+
+const sendEmail = async () => {
+  const info = await transporter.sendMail({
+    from: '"Hassan Ahmed 👻" <emmalee.ohara68@ethereal.email>', // sender address
+    to: "hassanahmed@gmail.com", // list of receivers
+    subject: "Subject Hello ✔", // Subject line
+    text: "123 Hello world?", // plain text body
+    html: "<b>1111 Hello world?</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // res.json({message: "email sent"})
+};
+
+export { addStudent, getStudent, getAllStudent , sendEmail};
--- src/routes/studentRoutes.js
+++ src/routes/studentRoutes.js
@@ -1,9 +1,12 @@
 import express from "express"
-import {addStudent} from "../controllers/studentContorller.js"
+import {addStudent, getAllStudent, getStudent, sendEmail} from "../controllers/studentContorller.js"
 
 
 const router = express.Router()
 
 router.post("/student" , addStudent)
+router.get("/student/:id" , getStudent)
+router.get("/allstudent" , getAllStudent)
+router.get("/sendemail" , sendEmail)
 
 export default router
